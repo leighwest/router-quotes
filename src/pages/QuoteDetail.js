3062eef9eaa1f5471de77e7dd618e5cf347405f7
Leigh West <leighwest@hotmail.com>
2022-10-01 05:26:21 +0000

    Added navigation pane

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,18 @@
-import { useParams } from 'react-router-dom';
+import Comments from 'components/comments/Comments';
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
 
 const QuoteDetail = () => {
   const params = useParams();
 
   return (
-    <>
-      <h1> Quote Detail Page</h1>
-      <p>{params.quoteId}</p>;
-    </>
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
   );
 };
 
