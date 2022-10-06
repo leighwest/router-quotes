6a1ed3557264d6932b022029311a0466c2ed70e6
Leigh West <leighwest@hotmail.com>
2022-10-06 21:20:34 +0000

    Hooked up create quote and get quotes functionality to firebase NoSQL server

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,11 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import Comments from 'components/comments/Comments';
+import useHttp from 'hooks/use-http';
+import { getSingleQuote } from 'lib/api';
 import HighlightedQuote from 'components/quotes/HighlightedQuote';
+import LoadingSpinner from 'components/UI/LoadingSpinner';
 
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
@@ -11,20 +14,42 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
-
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote
-        text={quote.text}
-        author={quote.author}
+        text={loadedQuote.text}
+        author={loadedQuote.author}
       />
       <Route
         path={`${match.path}`}
